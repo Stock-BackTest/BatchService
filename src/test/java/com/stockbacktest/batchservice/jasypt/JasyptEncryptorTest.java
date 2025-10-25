057e372709d2d74cbada39a9483534b2e6361868
doongdang <whtjrgus0428@gmail.com>
2025-10-25 20:13:17 +0900

    test: SpringBootTest 대신 JasyptConfig만 운용하게 변경

--- src/test/java/com/stockbacktest/batchservice/jasypt/JasyptEncryptorTest.java
+++ src/test/java/com/stockbacktest/batchservice/jasypt/JasyptEncryptorTest.java
@@ -1,13 +1,17 @@
 package com.stockbacktest.batchservice.jasypt;
 
+import com.stockbacktest.batchservice.config.JasyptConfig;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {JasyptConfig.class})
 class JasyptEncryptorTest {
 
   @Autowired
