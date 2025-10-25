dc2a701850f1a72b00063f1fec8f94163ad655f0
doongdang <whtjrgus0428@gmail.com>
2025-10-25 19:52:35 +0900

    add: @SpringBootApplication 어노테이션 추가

--- src/main/java/com/stockbacktest/batchservice/BatchserviceApplication.java
+++ src/main/java/com/stockbacktest/batchservice/BatchserviceApplication.java
@@ -1,7 +1,9 @@
 package com.stockbacktest.batchservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BatchserviceApplication {
 
   public static void main(String[] args) {
