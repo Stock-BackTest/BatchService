aef1973e99367d53cee199279ca45f44c8a92ced
doongdang <whtjrgus0428@gmail.com>
2025-10-28 17:03:02 +0900

    fix: profile 설정 yml에 기재

--- src/test/java/com/stockbacktest/batchservice/BatchserviceApplicationTests.java
+++ src/test/java/com/stockbacktest/batchservice/BatchserviceApplicationTests.java
@@ -2,10 +2,8 @@ package com.stockbacktest.batchservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class BatchserviceApplicationTests {
 
   @Test
