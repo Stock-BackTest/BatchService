c4f3849e9ce3a5270bd781c5f3fc9286b49871e7
j-yong98 <chlwodyd9829@gmail.com>
2025-12-25 16:46:48 +0900

    add: 기준일로 조회 기능 추가

--- src/main/java/com/stockbacktest/batchservice/job/etfdailypriceapi/domain/mapper/EtfDailyPriceMapper.java
+++ src/main/java/com/stockbacktest/batchservice/job/etfdailypriceapi/domain/mapper/EtfDailyPriceMapper.java
@@ -1,6 +1,8 @@
 package com.stockbacktest.batchservice.job.etfdailypriceapi.domain.mapper;
 
 import com.stockbacktest.batchservice.job.etfdailypriceapi.domain.entity.EtfDailyPrice;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -9,4 +11,5 @@ import org.apache.ibatis.annotations.Param;
 public interface EtfDailyPriceMapper {
 
   void insertDailyPrice(@Param("etfPrices") List<EtfDailyPrice> etfPrices);
+  List<EtfDailyPrice> findByBaseDate(LocalDate date);
 }
